f4fdc1d9e5bcd08697ac5757ce2d177c948ba129
dtarnawsky <damian@ionic.io>
2023-05-15 09:41:23 -0700

    fix

--- ios/capacitor-cordova-ios-plugins/sources/IonicEnterpriseAuth/IAC_TextRequestSerializer.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "IAC_AFURLRequestSerialization.h"
-
-@interface IAC_TextRequestSerializer : IAC_AFHTTPRequestSerializer
-
-+ (instancetype)serializer;
-
-@end
--- ios/capacitor-cordova-ios-plugins/sources/IonicEnterpriseAuth/IAC_TextResponseSerializer.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "IAC_AFURLResponseSerialization.h"
-
-@interface IAC_TextResponseSerializer : IAC_AFHTTPResponseSerializer
-
-+ (instancetype)serializer;
-
-@end
